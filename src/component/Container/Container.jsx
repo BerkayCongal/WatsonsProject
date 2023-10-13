11d292d439cec101cb16c805038bf642c8b9297f
Berkay Çongal <berkaycongal@hotmail.com>
2023-10-13 21:52:30 +0300

    main slidereklendi

--- src/component/Container/Container.jsx
+++ src/component/Container/Container.jsx
@@ -1,14 +1,31 @@
+import SliderCont from "../Sliderimg/Slider"
 import "./container.css"
 
+
+
 export default function Container() {
+   
     return(
-        <>
-            <div className="filter">
+        <> 
+            <div className="mainTop">
+                
+                <div className="filter">
+                    <h4><img src="/src/component/Container/img/sipari-takibi1.png" alt="" />Sipariş Takip</h4>
+                    <h4><img src="/src/component/Container/img/magazalar.png" alt="" />Mağazalar</h4>
+                    <h4> <img src="/src/component/Container/img/katalog.png" alt="" />Katalog</h4>
+                    <h4> <img src="/src/component/Container/img/teslimat-ve-deme.png" alt="" />Teslimat/Ödeme</h4>
+                    <h4> <img src="/src/component/Container/img/top-blog.avif" alt="" />Blog</h4>
+                </div>
+                <div className="headerlog">
+                    <img src="/src/component/Container/img/Pure-beuaty-.png" alt="" />
+                </div>
+                <div>
+                        <SliderCont/>
+                </div>
             
-                <h1>Deneme:</h1>
-               
-                 
             </div>
         </>
     )
 }
+
+
---
+++ src/component/Sliderimg/Slider.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import {LeftOutlined,RightOutlined} from '@ant-design/icons';
+import "./css/slider.css"
+
+
+
+
+export default function SliderCont()  {
+   const [sliderTransform, setSliderTransform] = useState(0);
+
+
+   function scollSlider () {
+    setSliderTransform()
+   }
+
+   return(
+    <>
+      <div className="slider-container">
+             <LeftOutlined  onClick={()=>{ sliderTransform < 0  ?  setSliderTransform(sliderTransform +14.4): null,console.log(sliderTransform)}} style={{position: "absolute", marginLeft: "30px", marginTop: "232px", zIndex: "301"}}/>
+            <RightOutlined onClick={()=>{sliderTransform >= -14.4  ?  setSliderTransform(sliderTransform -14.4): null,console.log(sliderTransform)}} style={{position: "absolute", marginLeft: "1350px", marginTop: "232px", zIndex: "301"}}/>
+        <div className="slider-main">
+        <img style={{transform:`translateX(${sliderTransform *100}px)`}} src="/src/component/Container/img/sliderimg1.png" alt="" />
+        <img style={{transform:`translateX(${sliderTransform *100}px)`}}src="/src/component/Container/img/sliderimg2.png" alt="" />
+         <img style={{transform:`translateX(${sliderTransform *100}px)`}} src="/src/component/Container/img/sliderimg3.png" alt="" />
+        </div>
+      </div>
+    </>
+   )
+}
