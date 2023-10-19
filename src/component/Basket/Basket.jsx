957c9b42af2b12fcf94df36b6b76cfc49b002309
Berkay Çongal <berkaycongal@hotmail.com>
2023-10-19 03:55:25 +0300

    Basket bitti animasyon eklendi...

--- src/component/Basket/Basket.jsx
+++ src/component/Basket/Basket.jsx
@@ -1,12 +1,20 @@
-import React from 'react'; 
+import React, { useRef } from 'react'; 
 import "./css/basket.css"
 import { Steps } from 'primereact/steps';
+import Cargo from"/src/Animations/Animation - 1697670627472.json"
+import { Link } from "react-router-dom";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+
+
 
 export default function Basket () {
-
+    const nameRef = useRef()
+    
     const items = [
         {
-            label: 'Ürün Paketleniyor'
+            label: 'Ürün Paketleniyor',
+           
         },
         {
             label: 'Kargoya verildi'
@@ -22,7 +30,44 @@ export default function Basket () {
              <Steps model={items} />
              
             </div>
-            <h1>Selam merhaba</h1>
+            
+           <div className='control-nav'>
+           <div className='container-link'>
+           <Link to={"/"}><p lassName='control-link'>
+            <Player 
+                    autoplay
+                    loop
+                    ref={nameRef}
+                    src={Cargo}
+                    style={{ height: '50px', width: '50px' }}
+                    >selam
+            </Player>Alışverişime Geri Dön
+            </p></Link>
+            <div>
+                <h1>Sepetindeki Ürünler</h1>
+            </div>
+           </div>
+            
+            
+            
+            <div className='card-price'>
+                <div>
+                    <div className='card-container'>
+                        <div className='all-price'>
+                            <span>Ara toplam</span>
+                            <span>0,00TL</span>
+                        </div>
+                        <div className='basketprice'>
+                            <h3>Toplam</h3>
+                            <span>0.00TL</span>
+                        </div>
+                    </div>
+                    <button className='pricebutton'>Alışverişi Tamamla</button>
+                </div>
+                
+            </div>
+           </div>
+                
         </>
     )
 }
